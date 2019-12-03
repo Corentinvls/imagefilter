b14fe2bf711e91089ee6151c46ee080944bac97b
Lrt95 <antony.correia@gmail.com>
2019-12-03 16:07:23 +0100

    add image + pdf backlog

---
+++ src/main/java/com/komdab/imagefilter/Filter.java
@@ -0,0 +1,12 @@
+package com.komdab.imagefilter;
+
+import org.bytedeco.opencv.opencv_core.*;
+import org.opencv.ximgproc.FastGlobalSmootherFilter;
+
+import static org.bytedeco.opencv.global.opencv_imgproc.*;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.*;
+
+
+public class Filter {
+
+}
