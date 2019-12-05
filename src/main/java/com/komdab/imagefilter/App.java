1d1047dfd18adf8fbf4b2caea9faebdc59582634
Lrt95 <antony.correia@gmail.com>
2019-12-05 11:55:23 +0100

    Hotfix exception done

--- src/main/java/com/komdab/imagefilter/App.java
+++ src/main/java/com/komdab/imagefilter/App.java
@@ -2,19 +2,9 @@ package com.komdab.imagefilter;
 
 
 public class App {
-    public static Logger logger;
     public static void main(String[] args) {
         //--filters "blur:3|grayscale" -i "imgs" -o "output_imgs";
-        logger = new Logger();
-        start(true);
         Commands.verifyCli(args);
-        start(false);
-    }
-
-    private static void start(boolean starting)
-    {
-        String s = starting ? "App imageFilter has started..." : "App imageFilter has finished...";
-        System.out.println(s);
-        logger.write(s);
+        Tools.annonce(false);
     }
 }
